eb29b4d3ba23ae434ddf26386526a4a495a298a7
Doug Bromley <doug.bromley@gmail.com>
2011-09-20 12:49:25 +0000

    Moving scores to left of reviewer name

--- tmpl/default.php
+++ tmpl/default.php
@@ -4,7 +4,14 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <ul class="movie-reviews">
 <?php foreach ($reviews as $review): ?>
     <?php if(isset($review->links->review) && isset($review->original_score)): ?>
-        <li><a href="<?php echo $review->links->review; ?>"><?php echo $review->publication; ?></a> (<?php echo $review->original_score; ?>)</li>
+        <li>(<?php echo $review->original_score; ?>) <a href="<?php echo $review->links->review; ?>"><?php echo $review->publication; ?></a></li>
     <?php endif; ?>
 <?php endforeach; ?>
 </ul>
+
+<style type="text/css">
+ul.movie-reviews {
+    list-style:none;
+    padding-left:0px;
+}
+</style>
